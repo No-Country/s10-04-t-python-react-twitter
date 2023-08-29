6b56e3d8c3f7a0e432194798fe0763b19be045f0
David Caballero <davidcame124@gmail.com>
2023-08-28 19:10:45 -0500

    Corrección de versión anterior. Implementación de notificaciones y sus respectivos métodos

--- back-end/twitter/applications/likes/models.py
+++ back-end/twitter/applications/likes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from ..tweets.models import Tweet, Comentario
+from ..users.models import User
 
 # Create your models here.
+
+class Notificacion(models.Model):
+    tipo = models.CharField(max_length=40)
+    de_usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='noti_de')
+    para_usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='noti_para')
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE, null=True, blank=True)
+    comentario = models.ForeignKey(Comentario, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_at']
--- back-end/twitter/applications/likes/views.py
+++ back-end/twitter/applications/likes/views.py
@@ -1,11 +1,13 @@
-from applications.users.models import User
-from applications.tweets.models import Tweet
+from ..users.models import User
+from ..tweets.models import Tweet
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializer import TweetSerializer
+from .serializer import TweetSerializer, NotiSerializer
+from .models import Notificacion
 
 # Create your views here.
 
+#Generar like
 @api_view(['POST'])
 def like(request, pk):
     tweet = Tweet.objects.get(pk=pk)
@@ -14,18 +16,58 @@ def like(request, pk):
         return Response({'status': 'ok'})
     else:
         tweet.liked.add(request.user)
+        if request.user != tweet.usuario:
+            Notificacion.objects.get_or_create(tipo='Le dio like a tu Tweet', tweet=tweet, para_usuario=tweet.usuario, de_usuario=request.user)
         return Response({'status': 'ok'})
-    
+
+#Likes de un Tweet 
 @api_view(['GET'])
 def list_like(_request, pk):
     tweets = Tweet.objects.filter(pk=pk)
     serializer = TweetSerializer(tweets, many=True)
     return Response(serializer.data)
 
-
+#Likes que dio el usuario
 @api_view(['GET'])
 def get_user_likes(_request, email):
     user = User.objects.get(email=email)
     tweets = Tweet.objects.filter(liked=user)
     serializer = TweetSerializer(tweets, many=True)
     return Response(serializer.data)
+
+
+### Notificaciones ############################################
+@api_view(['GET'])
+def noti(request):
+    user = request.user
+    notis = Notificacion.objects.filter(para_usuario=user, is_read=True)
+    serializer = NotiSerializer(notis, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def noti_no_read(request):
+    user = request.user
+    notis = Notificacion.objects.filter(para_usuario=user, is_read=False)
+    serializer = NotiSerializer(notis, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def noti_read_one(request, pk):
+    user = request.user
+    noti = Notificacion.objects.get(para_usuario=user, id=pk)
+    if noti.is_read == True: 
+        return Response({ 'message': 'Leído'})
+    else:
+        noti.is_read = True
+        noti.save()
+        return Response({ 'message': 'leído'})
+
+@api_view(['PUT'])
+def noti_read_all(request):
+    user = request.user
+    notis = Notificacion.objects.filter(para_usuario=user, is_read=False)
+    for noti in notis:
+        noti.is_read = True
+        noti.save()
+    return Response({ 'message': 'Todo leído'})
