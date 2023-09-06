dea52b0b7b0e4cd5e414ea54c11cf68970d6a83d
Aniokku <38799331+AnimeCommunity@users.noreply.github.com>
2023-09-06 11:05:46 -0500

    commit

--- back-end/twitter/twitter/settings/prod.py
+++ back-end/twitter/twitter/settings/prod.py
@@ -13,18 +13,14 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 # Data BAse
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgres://twitterdb_vjb4_user:83kttfsJFPk6FO8g7xDut311eVbt2hGP@dpg-cjs92ftm702s73fd9l70-a/twitterdb_vjb4',
-        conn_max_age=600
-    )
+    'default': dj_database_url.config()
 }
 
 # Direction off JS - IMG - CSS
 STATIC_URL = '/static/'
 STATIC_DIR = BASE_DIR.child('static')
 STATIC_ROOT = BASE_DIR.child('staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Direction off MEDIA
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.child('media')
