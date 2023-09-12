312f04c40306d3e5a96524ff4da5550fdcfe2029
Aniokku <38799331+AnimeCommunity@users.noreply.github.com>
2023-09-12 14:03:04 -0500

    Update serializer.py

--- back-end/twitter/applications/tweets/serializer.py
+++ back-end/twitter/applications/tweets/serializer.py
@@ -2,9 +2,50 @@ from rest_framework import serializers, pagination
 from .models import Tweet, Cita, Comentario
 from applications.users.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+                'id', 
+                'email', 
+                'firs_name', 
+                'last_name',
+                'avatar',
+                ]
 
 
-class TweetSerializer(serializers.ModelSerializer):
+class TweetSerializerGET(serializers.ModelSerializer):
+    
+    liked_count = serializers.SerializerMethodField( read_only=True)
+    comentario_count = serializers.SerializerMethodField()
+    retweet = serializers.SerializerMethodField()
+    usuario = UserSerializer()
+
+    class Meta:
+        model = Tweet
+        fields = [
+            'id',
+            'usuario',
+            'contenido',
+            'multimedia',
+            'gif',
+            'created',
+            'liked_count',  
+            'comentario_count', 
+            'retweet', 
+        ]
+        
+    def get_liked_count(self, obj):
+        return obj.liked.all().count()
+
+    def get_comentario_count(self, obj):
+        return obj.comentario.count()
+    
+    def get_retweet(self, obj):
+        return obj.retweet.count()
+   
+        
+class TweetSerializerPOST(serializers.ModelSerializer):
     
     liked_count = serializers.SerializerMethodField( read_only=True)
     comentario_count = serializers.SerializerMethodField()
@@ -32,16 +73,10 @@ class TweetSerializer(serializers.ModelSerializer):
     
     def get_retweet(self, obj):
         return obj.retweet.count()
-        
-        
-class CitaSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Cita
-        fields = ('__all__')
-        
 
-class ComentarioSerializer(serializers.ModelSerializer):
+class ComentarioSerializerGET(serializers.ModelSerializer):
+    
+    usuario = UserSerializer()
     
     class Meta:
         model = Comentario
@@ -55,13 +90,36 @@ class ComentarioSerializer(serializers.ModelSerializer):
         'gif',
     ]
 
+class ComentarioSerializerPOST(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Comentario
+        fields = [
+        'id', 
+        'usuario', 
+        'tweet_original', 
+        'content', 
+        'created_at',
+        'multimedia',
+        'gif',
+    ]
+      
+        
+class CitaSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Cita
+        fields = ('__all__')
+        
  
 class TweetSerializerId(serializers.ModelSerializer):
     
     liked_count = serializers.SerializerMethodField( read_only=True)
     comentario_count = serializers.SerializerMethodField()
-    comentario = ComentarioSerializer(many=True)
+    comentario = ComentarioSerializerGET(many=True)
     retweet = serializers.SerializerMethodField()
+    usuario = UserSerializer()
+    
     class Meta:
         model = Tweet
         fields = [
